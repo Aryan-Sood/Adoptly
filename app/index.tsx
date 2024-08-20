0efdc4682ee9e715a615e0ac6b9b9b026adc76d6
Aryan-Sood <aryansood12@gmail.com>
2024-08-21 03:08:27 +0530

    Expo init

---
+++ app/index.tsx
@@ -0,0 +1,25 @@
+import { Text, View } from "react-native";
+import { useFonts } from "expo-font";
+
+
+export default function Index() {
+
+  useFonts({
+    'outfit': require('../assets/fonts/Outfit-Regular.ttf'),
+    'outfit-bold': require('../assets/fonts/Outfit-Bold.ttf'),
+    'outfit-medium': require('../assets/fonts/Outfit-Medium.ttf')
+  })
+
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
---
+++ screens/Intro/IntroScreen.js
@@ -0,0 +1,19 @@
+import { View, Text, SafeAreaView } from 'react-native'
+import React from 'react'
+
+export default function IntroScreen() {
+
+
+
+
+  return (
+    <>
+    <SafeAreaView />
+    <View>
+
+    </View>
+    </>
+  )
+}
+
+
